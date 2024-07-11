e6ac76b72025b4ec53a61a54072f37a48e195dfa
SebastianVergaraVelasquez <97114145+SebastianVergaraVelasquez@users.noreply.github.com>
2024-07-11 07:08:45 -0500

    feat: :tada: Initialize BetPlayApp Spring

---
+++ src/main/java/com/ligabetplay/betplayapp/BetplayappApplication.java
@@ -0,0 +1,13 @@
+package com.ligabetplay.betplayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetplayappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetplayappApplication.class, args);
+	}
+
+}
